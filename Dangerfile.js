041b3119707f34a14160a2ce2d4e9145e0221c70
grausof <grausof@gmail.com>
2022-06-06 11:35:08 +0200

    danger-plugin integration

---
+++ Dangerfile.js
@@ -0,0 +1,41 @@
+const { schedule } = require("danger");
+const { customRules } = require("@pagopa/danger-plugin");
+
+const recordScope = {
+  projectToScope: {
+    2449547: "Bonus Vacanze",
+    2463683: "My Portal",
+    2477137: "Bonus Pagamenti Digitali",
+    IAC: "Bonus Pagamenti Digitali",
+    IOACGN: "Carta Giovani Nazionale",
+    IASV: "Sicilia Vola",
+    IAGP: "EU Covid Certificate",
+    IARS: "Redesign Servizi",
+    ASZ: "Zendesk",
+    IAMVL: "Piattaforma Notifiche",
+    IAFIMS: "Federated Identity Management System",
+    AP: "Carta della cultura",
+    SFEQS: "Firma con IO",
+  },
+  tagToScope: {
+    android: "Android",
+    ios: "iOS",
+    bonus_vacanze: "Bonus Vacanze",
+    messages: "Messages",
+    payments: "Payments",
+    services: "Services",
+    profile: "Profile",
+    privacy: "Privacy",
+    security: "Security",
+    accessibility: "Accessibility",
+    bpd: "Bonus Pagamenti Digitali",
+    cgn: "Carta Giovani Nazionale",
+    fims: "Federated Identity Management System",
+    myportal: "My Portal",
+    development: "Development",
+    backend: "Backend",
+  },
+};
+
+// Using schedule because this is an async task
+schedule(customRules(recordScope));
