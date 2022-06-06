7b59f4b222a10723b633d302192a4876d5f3a905
grausof <grausof@gmail.com>
2022-06-06 15:28:55 +0200

    Add firma con IO recordScope

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,3 +1,14 @@
 const customRules = require("@pagopa/danger-plugin");
 
+const recordScope = {
+  projectToScope: {
+    SFEQS: "Firma con IO"
+  },
+  tagToScope: {
+    development: "Development",
+    backend: "Backend",
+    dependency: "Dependency",
+  },
+};
+
 customRules(recordScope);
